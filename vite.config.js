aacd5315744abb77c9b7141caddcbe5220a3751f
xinzzu <azis2100016101@webmail.uad.ac.id>
2025-08-15 19:04:54 +0700

    Fix Vite assets for Railway production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
-
 
 export default defineConfig({
     plugins: [
@@ -9,6 +7,15 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss()
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css'
+            }
+        }
+    }
 });
